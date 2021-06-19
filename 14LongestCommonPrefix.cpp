d68eabcb30a4c3949b61ec88e9f71dc795579861
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2021-06-19 17:05:32 -0500

    finished, 4ms

--- 14LongestCommonPrefix.cpp
+++ 14LongestCommonPrefix.cpp
@@ -3,37 +3,46 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string a ="";
         int size = 201;
-        string help="";
-        string shorter ="";
-        int cont =0;
-        for(int i=0;i<strs.size();i++){
-            if(strs[i].length()<size){
-                size=strs[i].length();
-                shorter = strs[i];
+     string help="";
+     string shorter ="";
+     int cont =0;
+     for(int i=0;i<strs.size();i++){
+         if(strs[i].length()<size){
+             size=strs[i].length();
+             shorter = strs[i];
 
-            }
-        }
-        for(int i=0;i<strs.size();i++){
-            
-            for(int j=0;j<size;j++){
-                if(strs[i].at(j)==shorter.at(j)){
-                    help+=strs[i];
-                }
-            }if(help!=""){
-                if(help.length()<=a.length()){
-                    a=help;
-                }
-                cont++;
-                
-            }else{
-                    break;
-                }           
-            
-        }
-        if(cont==strs.size())
-            return a;
-        else
-            return "";        
-        
+         }
+     }
+     a=shorter;
+     for(int i=0;i<strs.size();i++){
+
+         for(int j=0;j<size;j++){
+             if(strs[i].at(j)==shorter.at(j)){
+                 help+=strs[i].at(j);
+
+             }else{
+                 break;
+             }
+         }
+         if(help.length()>0){
+
+             if(help.length()<=a.length()){
+                  a=help;
+             }
+             help="";
+             cont++;
+
+         }else
+             break;
+
+
+     }
+     if(cont==strs.size())
+         return a;
+     else
+         return "";
+
+
+
     }
 };
