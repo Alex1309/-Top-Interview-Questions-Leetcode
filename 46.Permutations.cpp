6149645cf0521ee8157a35732bf26ad86d7ac9ec
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2021-07-02 23:10:01 -0500

    Add files via upload

--- 46.Permutations.cpp
+++ 46.Permutations.cpp
@@ -13,3 +13,28 @@ public:
         return result;
     }
 };
+
+//Other opcion
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        permutation(nums,0,result);
+        return result;
+       
+    }
+    void permutation(vector<int>&nums,int a, vector<vector<int>>& result){
+        int k=a;
+
+        if(k==nums.size()-1){
+            result.push_back(nums);
+        }
+        for(int i=k;i<nums.size();i++){
+            swap(nums[i],nums[k]);
+            permutation(nums,k+1,result);
+            swap(nums[i],nums[k]);
+
+        }
+        
+    }
+};
