0728b145adc324a0d40f98d175aa7492dc2747fc
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2021-07-08 00:51:08 -0500

    Finished 36 ms

--- 49.GroupAnagrams.cpp
+++ 49.GroupAnagrams.cpp
@@ -43,3 +43,45 @@ public:
 	return sol;
     }
 };
+
+//Finished 36 ms
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+	vector<vector<string> > sol;
+	vector<string> temp;
+
+
+	multimap <string, string> st;
+
+	for(int i=0;i<strs.size();i++){
+		string aux = strs[i];
+		sort(aux.begin(), aux.end());
+		st.insert(pair <string, string> (aux, strs[i]));
+
+	}
+	multimap <string, string> :: iterator itr;
+	multimap <string, string> :: iterator itrO;
+
+	vector<string> keys;
+	for( auto it = st.begin(), end = st.end();
+			it != end;
+			it = st.upper_bound(it->first)
+	)    keys.push_back(it->first);
+
+
+	for(int i=0;i<keys.size();i++){
+
+		typedef multimap<string,string> stAux;
+		std::pair<stAux::iterator, stAux::iterator> range;
+
+		range = st.equal_range(keys[i]);
+		for (stAux::iterator it = range.first; it != range.second; ++it){
+			temp.push_back(it->second);
+		}
+		sol.push_back(temp);
+		temp.clear();
+	}
+	return sol;
+    } 
+};
