d5dcf2ef52a396e1a518d06b034ba2b7a8f34b37
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2021-07-15 00:42:01 -0500

    Other solution 0 ms

--- 75.SortColors.cpp
+++ 75.SortColors.cpp
@@ -40,3 +40,27 @@ public:
         
     }
 };
+
+// other solution 0 ms
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+    map<int,int> n;
+
+        if(nums.size()>1){
+            for(int i=0;i<nums.size();i++){
+				n[nums[i]]++;
+            }
+            n[1]+=n[0];
+            n[2]+=n[1];
+
+	for(int i=0;i<nums.size();i++){
+		if(i<n[0]){
+            nums[i]=0;}
+		else if(i<n[1]){
+			nums[i]=1;
+		}else if(i<n[2])
+			nums[i]=2;}
+        }
+    }
+};
