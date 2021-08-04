0908c22216a7886b53f64c4747499065e6f19bf4
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2021-08-04 01:07:04 -0500

    Finished 4 ms

--- 38.CountAndSay.cpp
+++ 38.CountAndSay.cpp
@@ -1,17 +1,7 @@
-//============================================================================
-// Name        : .cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
-#include <iostream>
-using namespace std;
-#include <string>
-
-string countAux(string a){
-	string b="";
+class Solution {
+public:
+    string countAux(string a){
+        string b="";
 	int con=0;
 	int v=0;
 	for(int i=0;i<a.length();i++){
@@ -23,7 +13,6 @@ string countAux(string a){
 			}
 			else{
 				b +=(to_string(1)+a[i]);
-
 			}
 		}
 		else if(a[i]==a[i+1]){
@@ -32,22 +21,26 @@ string countAux(string a){
 		}else{
 			if(con==0)
 				b +=(to_string(1)+a[i]);
-			else
+			if(v==1){
+				con+=1;
 				b +=(to_string(con)+a[i]);
+			}
 			v=0;
+			con=0;
 		}
-
 	}
 	return b;
+    }
+    string countAndSay(int n) {
+       
+	string f[n+1];
 
-}
-string countAndSay(int n) {
+	f[1] = "1";
 
-
-	return "!";
-}
-
-int main() {
-
-	cout<<countAux("1211");
-}
+	for (int i = 2; i <= n; i++)
+	{
+		f[i] = countAux(f[i-1]);
+	}
+	return f[n];
+    }
+};
