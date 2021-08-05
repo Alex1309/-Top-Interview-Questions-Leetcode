0ca3a1850d749c723dd5ef1290e3c49b55ea0cc2
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2021-08-05 01:25:42 -0500

    Finished 84 ms

--- 68.sqrt(x).cpp
+++ 68.sqrt(x).cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        	int total=50000;
-	int c[total];
-	int a[total];
+    	int total=50000;
+		int c[total];
+		int a[total];
+		int max = 2147483646;
+		int count = 3;
+		c[0]=1;
+		a[0]=1;
+		int t = x/2;
 
-	int count = 3;
-	c[0]=1;
-	a[0]=1;
-	int t = x/2;
-
-	if(t>total)
-		t=total;
-	int i=0;
-	while(x>=c[i]){
-		i++;
-		c[i]=c[i-1]+count;
-		count+=2;
-		a[i]=i+1;
-	}
+		if(t>total)
+			t=total;
+		int i=0;
+		int v=0;
+		while(x>=c[i]){
+			if(c[i]>=max-count){
+				v=1;
+				break;
+			}
+			i++;
+			c[i]=c[i-1]+count;
+			count+=2;
+			a[i]=i+1;
+		}
 	if(x==0)
 		return 0;
+	if(v==1)
+		return a[i];
 	return a[i-1];
     }
 };
