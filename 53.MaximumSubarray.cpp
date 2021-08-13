2b79435f70e2c61ea9e95900ee90baf4a0ccc651
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2021-08-12 23:52:40 -0500

    Finished 4 ms

--- 53.MaximumSubarray.cpp
+++ 53.MaximumSubarray.cpp
@@ -33,3 +33,19 @@ public:
 	return sums[sums.size()-1];
 }
 };
+
+//4 ms
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+	int sum=0;
+    int mayor =nums[0];
+	for(int i=0;i<nums.size();i++){
+		sum+=nums[i];		
+        mayor =max(sum,mayor);
+		if(sum<0)
+			sum=0;
+	}
+	return mayor;
+}
+};
